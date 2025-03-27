0ccb1c66947540d5f35bd8b4079a10a06f2da679
AngCarlos-2 <angeloforswitch@gmail.com>
2025-03-27 02:30:07 -0300

    feat: email verifying
    Verificação do email.

--- Desafio 3/projeto/scripts/main.js
+++ Desafio 3/projeto/scripts/main.js
@@ -5,3 +5,41 @@ function cancelar() {
 function confirmar() {
     alert("Inscrição confirmada");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const emailInput = document.getElementById("email");
+    const emailError = document.querySelector(".erro-email");
+
+    emailError.style.display = "none"; 
+
+    emailInput.addEventListener("input", () => {
+        const emailValue = emailInput.value.trim();
+        const endsWithEmailCom = emailValue.endsWith("@email.com");
+        const notOnlyNumbers = !/^\d+$/.test(emailValue);
+        const hasContent = emailValue.length > 0;
+
+        if (endsWithEmailCom && notOnlyNumbers && hasContent) {
+            emailError.style.display = "none"; 
+            emailInput.style.borderColor = ""; 
+        } else {
+            emailError.style.display = "block"; 
+            emailInput.style.borderColor = "red"; 
+        }
+    });
+
+    /*const textBox = document.getElementsByClassName("caixadetexto");
+
+    textBox.addEventListener("input", () => {
+        const textBoxValue = textBox.value.trim();
+
+        if (textBoxValue.length > 0) {
+            emailError.style.display = "none"; 
+            emailInput.style.borderColor = ""; 
+        } else {
+            emailError.style.display = "block"; 
+            emailInput.style.borderColor = "red"; 
+        }
+    })*/
+});
+
+
